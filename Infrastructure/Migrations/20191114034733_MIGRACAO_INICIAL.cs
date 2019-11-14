7a431b6a6a7871970690da20bfb348233c6de9a3
Raí Lima <rai160493@hotmail.com>
2019-11-14 00:50:31 -0300

    Cria config inicial das migrations

---
+++ Infrastructure/Migrations/20191114034733_MIGRACAO_INICIAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class MIGRACAO_INICIAL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Compra",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(maxLength: 100, nullable: false),
+                    JurosSimples = table.Column<decimal>(type: "decimal(4, 4)", nullable: true),
+                    JurosCompostos = table.Column<decimal>(type: "decimal(4, 4)", nullable: true),
+                    Data = table.Column<DateTime>(nullable: false),
+                    Parcelas = table.Column<int>(nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(4, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compra", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compra");
+        }
+    }
+}
