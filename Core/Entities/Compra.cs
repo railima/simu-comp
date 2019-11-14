a7856deb6cc9ccdeab5a78493b6fab7c6427db0d
Raí Lima <rai160493@hotmail.com>
2019-11-14 00:50:18 -0300

    Insere annotations na classe Compra

--- Core/Entities/Compra.cs
+++ Core/Entities/Compra.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Core.Entities
@@ -7,10 +9,19 @@ namespace Core.Entities
     public class Compra
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Descricao { get; set; }
-        public decimal JurosSimples { get; set; }
-        public decimal JurosCompostos { get; set; }
+        [Column(TypeName = "decimal(4, 4)")]
+        public decimal? JurosSimples { get; set; }
+        [Column(TypeName = "decimal(4, 4)")]
+        public decimal? JurosCompostos { get; set; }
+        [Required]
         public DateTime Data { get; set; }
+        [Required]
         public int Parcelas { get; set; }
+        [Column(TypeName = "decimal(4, 2)")]
+        [Required]
+        public decimal Valor { get; set; }
     }
 }
