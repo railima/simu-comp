66a9970aa16709ad46e3ce73f3c30cb711570e31
Raí Lima <rai160493@hotmail.com>
2019-11-14 00:09:09 -0300

    Cria dbContext

---
+++ Infrastructure/DataAccess/CompraContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.DataAccess
+{
+    public class CompraContext: DbContext
+    {
+        public CompraContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Compra> Compras { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Compra>().ToTable("Compra");
+        }
+
+    }
+}
