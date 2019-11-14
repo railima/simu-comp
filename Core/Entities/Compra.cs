3d68efc8d280e50ea3276fc3fb69769e29654978
Raí Lima <rai160493@hotmail.com>
2019-11-14 00:08:56 -0300

    Cria entidade Compra

---
+++ Core/Entities/Compra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Compra
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal JurosSimples { get; set; }
+        public decimal JurosCompostos { get; set; }
+        public DateTime Data { get; set; }
+        public int Parcelas { get; set; }
+    }
+}
