4f69d94a25a9302419ace40cd3575f8234423437
Raí Lima <rai160493@hotmail.com>
2019-11-15 02:54:03 -0300

    Add compraDTO

---
+++ Core/DTOs/CompraDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Entities;
+
+namespace Core.DTOs
+{
+    public class CompraDTO
+    {
+        public CompraDTO()
+        {
+        }
+        public int? Id { get; set; }
+        public string Descricao { get; set; }
+        public double Juros { get; set; }
+        public DateTime Data { get; set; }
+        public int QuantidadeParcela { get; set; }
+        public double Valor { get; set; }
+        public double ValorTotalJurosSimples { get; set; }
+        public double ValorTotalJurosComposto { get; set; }
+        public ICollection<ParcelaInfo> Parcelas { get; set; }
+    }
+    public class ParcelaInfo
+    {
+        public DateTime DataVencimento { get; set; }
+        public double ValorJurosSimples { get; set; }
+        public double ValorJurosComposto { get; set; }
+
+    }
+}
+
