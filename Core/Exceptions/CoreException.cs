15e8b07867822adb7c1609bcb9f7ffa843ef273e
Raí Lima <rai160493@hotmail.com>
2019-11-15 02:53:07 -0300

    Add classes de exceptions

---
+++ Core/Exceptions/CoreException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Exceptions
+{
+    public class CoreException : Exception
+    {
+        internal CoreException(string businessMessage)
+               : base(businessMessage)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Exceptions/InfrastructureException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructure.Exceptions
+{
+    public class InfrastructureException : Exception
+    {
+        internal InfrastructureException(string businessMessage)
+               : base(businessMessage)
+        {
+        }
+    }
+}
